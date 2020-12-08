bd59ec2addfb4d6e9ab6bde036f38e4e76f6324f
Jacob Needels <jacobneedels@DN51t5kb.SUNet>
2020-12-08 15:36:50 -0800

    Added axisymm source terms to energy equations for 2T model

--- SU2_CFD/src/numerics/NEMO/NEMO_sources.cpp
+++ SU2_CFD/src/numerics/NEMO/NEMO_sources.cpp
@@ -296,7 +296,20 @@ CNumerics::ResidualType<> CSource_NEMO::ComputeVibRelaxation(const CConfig *conf
 CNumerics::ResidualType<> CSource_NEMO::ComputeAxisymmetric(const CConfig *config) {
 
   unsigned short iDim, iSpecies, iVar;
-  su2double rho, rhov, vel2, H, yinv;
+  su2double rho, rhov, vel2, H, yinv, T, Tve, sumJhs_y, sumJeve_y, qy_ve;
+  su2double *Ds, *V, **GV, mu, ktr, kve, div_vel;
+  
+  bool viscous = config->GetViscous();
+  bool rans = (config->GetKind_Turb_Model() != NONE);
+
+  ktr = Thermal_Conductivity_i;
+  kve = Thermal_Conductivity_ve_i;
+  rho = V_i[RHO_INDEX];
+  T   = V_i[T_INDEX];
+  Tve  = V_i[TVE_INDEX];
+  GV  = PrimVar_Grad_i;
+
+  hs = fluidmodel->GetSpeciesEnthalpy(T, Tve, eve_i);
 
     /*--- Initialize residual and Jacobian arrays ---*/
   for (iVar = 0; iVar < nVar; iVar++) {
@@ -312,6 +325,7 @@ CNumerics::ResidualType<> CSource_NEMO::ComputeAxisymmetric(const CConfig *confi
   rhov   = U_i[nSpecies+1];
   H      = V_i[H_INDEX];
   vel2   = 0.0;
+
   for (iDim = 0; iDim < nDim; iDim++)
     vel2 += V_i[VEL_INDEX+iDim]*V_i[VEL_INDEX+iDim];
   for (iSpecies = 0; iSpecies < nSpecies; iSpecies++)
@@ -338,18 +352,32 @@ CNumerics::ResidualType<> CSource_NEMO::ComputeAxisymmetric(const CConfig *confi
     su2double u = U_i[1]/U_i[0];
     su2double v = U_i[2]/U_i[0];
 
+    qy_ve = kve*GV[TVE_INDEX][1];
+
+      for (iSpecies = 0; iSpecies < nHeavy; iSpecies++) { 
+        sumJhs_y += (rho*Diffusion_Coeff_i[iSpecies]*GV[RHOS_INDEX+iSpecies][1]
+        - V_i[RHOS_INDEX+iSpecies]*Vector[1]) * hs[iSpecies];
+
+        sumJeve_y += (rho*Diffusion_Coeff_i[iSpecies]*GV[RHOS_INDEX+iSpecies][1]
+        - V_i[RHOS_INDEX+iSpecies]*Vector[1]) * eve_i[iSpecies];
+      }
+
     for (iSpecies = 0; iSpecies < nSpecies; iSpecies++)
       residual[iSpecies] -= 0.0;
+
     residual[nSpecies] -= Volume*(yinv*total_viscosity_i*(PrimVar_Grad_i[1][1]+PrimVar_Grad_i[2][0]) 
                                                                 -TWO3*AuxVar_Grad_i[0][0]);
+
     residual[nSpecies+1] -= Volume*(yinv*total_viscosity_i*2*(PrimVar_Grad_i[2][1]-v*yinv)
                                                                 -TWO3*AuxVar_Grad_i[0][1]);
-    residual[nSpecies+2] = Volume*(yinv*(total_viscosity_i*(u*(PrimVar_Grad_i[2][0]+PrimVar_Grad_i[1][1])
+
+    residual[nSpecies+2] -= Volume*(yinv*(- sumJhs_y + total_viscosity_i*(u*(PrimVar_Grad_i[2][0]+PrimVar_Grad_i[1][1])
                                                  +v*TWO3*(2*PrimVar_Grad_i[1][1]-PrimVar_Grad_i[1][0]
                                                  -v*yinv+U_i[0]*turb_ke_i))
                                                  -total_conductivity_i*PrimVar_Grad_i[0][1])
-                                                 -TWO3*(AuxVar_Grad_i[1][1]+AuxVar_Grad_i[2][1]));;
-    residual[nSpecies+3] = 0;
+                                                 -TWO3*(AuxVar_Grad_i[1][1]+AuxVar_Grad_i[2][1]));
+
+    residual[nSpecies+3] -= Volume*(yinv*(-sumJeve_y -qy_ve));
   }
 
 //  if (implicit) {
